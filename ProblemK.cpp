dc018c076011a16fe949106b0b6130a3b750d0a2
Omar <omaar_5@hotmail.com>
2022-04-07 17:04:46 +0200

    solved Problem K

---
+++ ProblemK.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << min(a, min(b, c)) << " " << max(a, max(b, c)) << endl;
+}
