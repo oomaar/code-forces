3edf9fb3ed05016d1080287281eaa7abd03c6c9b
Omar <omaar_5@hotmail.com>
2022-04-07 17:00:37 +0200

    solved Problem C

---
+++ ProblemC.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long x;
+    long long y;
+
+    cin >> x >> y;
+
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+
+    return 0;
+}
