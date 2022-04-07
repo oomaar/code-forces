9b97b7b8e8c413209aa772bae6394aac0a566253
Omar <omaar_5@hotmail.com>
2022-04-07 17:00:48 +0200

    solved Problem E

---
+++ ProblemE.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << fixed << setprecision(9);
+    double R;
+
+    cin >> R;
+
+    double pi = 3.141592653;
+    double sqrtR = R * R;
+
+    cout << pi * sqrtR << endl;
+    return 0;
+}
