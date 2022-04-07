7414ea8971a7c406a24ccb6e285d7641ab503a91
Omar <omaar_5@hotmail.com>
2022-04-07 17:00:32 +0200

    solved Problem B

---
+++ ProblemB.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int number;
+    long long longNumber;
+    char letter;
+    float naturalNumber;
+    long double doubleNumber;
+    
+    cin >> number >> longNumber >> letter >> naturalNumber >> doubleNumber;
+    cout << number << endl << longNumber << endl << letter << endl << naturalNumber << endl << doubleNumber;
+    
+    return 0;
+}
