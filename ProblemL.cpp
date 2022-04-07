cee5d6b93f8d96f5096e2bffbdbe70fbcad6b444
Omar <omaar_5@hotmail.com>
2022-04-07 17:04:52 +0200

    solved Problem L

---
+++ ProblemL.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string person1First, person1Second;
+    string person2First, person2Second;
+
+    cin >> person1First >> person1Second;
+    cin >> person2First >> person2Second;
+
+    if (person1Second == person2Second)
+    {
+        cout << "ARE Brothers" << endl;
+    }
+    else
+    {
+        cout << "NOT" << endl;
+    }
+
+    return 0;
+}
