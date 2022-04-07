8405c2317645933293cfaea74dfff7bec94cdded
Omar <omaar_5@hotmail.com>
2022-04-07 17:04:35 +0200

    solved Problem J

---
+++ ProblemJ.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+
+    cin >> A >> B;
+
+    if (B % A == 0 || A % B == 0)
+    {
+        cout << "Multiples" << endl;
+    }
+    else
+    {
+        cout << "No Multiples" << endl;
+    }
+
+    return 0;
+}
