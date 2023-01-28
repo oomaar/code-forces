d81e084fcf61f65570278c090a9b12c2bae2c9cf
omaar <omaar_5@hotmail.com>
2023-01-28 21:57:16 +0200

    solution in c++

---
+++ problem-set/Problem-A-Domino-Piling/Domino-Piling.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int length;
+    int width;
+    
+    cin >> length;
+    cin >> width;
+    
+    int result = length * width;
+    
+    cout << result / 2;
+}
